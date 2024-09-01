ff56bd17a053c5f2d2eada229bdda3baac69da66
thimller <130771408+thimller@users.noreply.github.com>
2024-09-01 18:44:52 -0300

    Add files via upload

---
+++ AtividadePortifolio/script.js
@@ -0,0 +1,9 @@
+//rolar até o local da pagina definido
+function rolar(objID) {
+    this.location = "#" + objID;
+}
+
+//alerta de envio de contato
+function enviado(){
+    alert("Contato enviado")
+}
